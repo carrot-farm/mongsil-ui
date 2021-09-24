0f31a2411d2689451510cf655b76259bf716e1d8
suhyun <chohoki@gmail.com>
2021-09-24 20:42:47 +0900

    feat: emboss, dent style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import * as React from 'react';
 
+// import './App.css';
+
 function App(): any {
-  return <div className="p-6 bg-red-500 text-white">hello World!</div>;
+  return (
+    <div className="app bg-base">
+      <div className="p-5 flex">
+        <div className="w-20 h-20 bg-base emboss-100 "></div>
+        <div className="w-20 h-20 bg-base emboss-200 ml-10"></div>
+        <div className="w-20 h-20 bg-base emboss-300 ml-10"></div>
+        <div className="w-20 h-20 bg-base emboss-400 ml-10"></div>
+        <div className="w-20 h-20 bg-base emboss-500 ml-10"></div>
+      </div>
+      <div className="p-5 flex ">
+        <div className="w-20 h-20 bg-base dent-100 "></div>
+        <div className="w-20 h-20 bg-base dent-200 ml-10"></div>
+        <div className="w-20 h-20 bg-base dent-300 ml-10"></div>
+        <div className="w-20 h-20 bg-base dent-400 ml-10"></div>
+        <div className="w-20 h-20 bg-base dent-500 ml-10"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
