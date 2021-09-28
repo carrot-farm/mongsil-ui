3b0ddf70c09f9340bfc108749dfdab770987dc60
suhyun <chohoki@gmail.com>
2021-09-28 20:42:39 +0900

    feat: button component

--- src/components/Button/Buttons.tsx
+++ src/components/Button/Buttons.tsx
@@ -1,11 +1,55 @@
 import * as React from 'react';
 
 interface ButtonProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-function Button({ children }: ButtonProps): JSX.Element {
-  return <button>{children}</button>;
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, ...args }, ref) => {
+    const [isPressed, setIsPressed] = React.useState<boolean>(false);
+    const [isAnimationEnd, setIsAnimationEnd] = React.useState<boolean>(true);
+    const [ing, setIng] = React.useState<boolean>(false);
+
+    const pressed = React.useCallback(() => {
+      setIsPressed(true);
+      setIsAnimationEnd(false);
+      setIng(true);
+    }, []);
+
+    const unPressed = React.useCallback(() => {
+      setIsPressed(false);
+    }, []);
+
+    const handleAnimationEnd = React.useCallback(() => {
+      setIsAnimationEnd(true);
+    }, []);
+
+    React.useEffect(() => {
+      if (ing === false) {
+        return;
+      }
+      if (isPressed === false && isAnimationEnd === true) {
+        setIng(false);
+      }
+    }, [isPressed, isAnimationEnd, ing]);
+
+    return (
+      <button
+        className={`button ${ing ? 'animate-press-300-0' : ''} ${className ?? ''}`}
+        onMouseDown={pressed}
+        onMouseUp={unPressed}
+        onTouchStart={pressed}
+        onTouchEnd={unPressed}
+        onAnimationEnd={handleAnimationEnd}
+        {...args}
+        ref={ref}
+      >
+        <div className={ing ? 'animate-scale-1-09' : ''}>{children}</div>
+      </button>
+    );
+  },
+);
 
 export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
+      // # color
       colors: {
         base: {
           DEFAULT: 'var(--color-base)',
@@ -13,6 +14,35 @@ module.exports = {
         secondary: {
           DEFAULT: 'var(--color-secondary)',
         },
+      },
+      // # keyframes
+      keyframes: {
+        'press-300-0': {
+          '0%': {
+            'box-shadow': `3px 3px 3px hsl(var(--color-base-deg) 3% 5% / 6%),
+              5px 5px 12px hsl(var(--color-base-deg) 3% 5% / 10%),
+              -3px -3px 3px hsl(var(--color-base-deg) 3% 100% / 80%),
+              -5px -5px 12px hsl(var(--color-base-deg) 5% 100% / 80%);`,
+          },
+          '100%': {
+            'box-shadow': `0px 0px 0px hsl(var(--color-base-deg) 3% 5% / 6%),
+              0px 0px 0px hsl(var(--color-base-deg) 3% 5% / 10%),
+              0px 0px 0px hsl(var(--color-base-deg) 3% 100% / 80%),
+              0px 0px 0px hsl(var(--color-base-deg) 5% 100% / 80%);`,
+          },
+        },
+        'scale-1-09': {
+          '0%': {
+            transform: 'scale(1)',
+          },
+          '100%': {
+            transform: 'scale(0.94)',
+          },
+        },
+      },
+      animation: {
+        'press-300-0': 'press-300-0 200ms cubic-bezier(0.25, 1, 0.5, 1) forwards',
+        'scale-1-09': 'scale-1-09 200ms cubic-bezier(0.25, 1, 0.5, 1) forwards',
       },
     },
   },
