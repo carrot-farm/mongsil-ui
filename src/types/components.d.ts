b8c67c76015d6aa2f540bfd61dc99d9290752cf2
suhyun <chohoki@gmail.com>
2021-11-07 20:30:15 +0900

    feat: storybook 적용

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -2,7 +2,7 @@
 export type StateBind = 'none' | 'stateOnly' | 'both';
 export type ValueTypes = string | string[] | boolean | number | undefined;
 export type Values = Record<string, ValueTypes>;
-export type InputChange = (value?: ValueTypes, name) => void | false;
+export type InputChange = (value?: ValueTypes, name?: string) => void | false;
 export type Direction = 'x' | 'y';
 
 /** ===== rule  ===== */
