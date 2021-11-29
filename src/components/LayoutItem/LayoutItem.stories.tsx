fe7ded95f67b0b52a725dfcec2a18a5aff1d9173
suhyun <chohoki@gmail.com>
2021-11-29 19:27:38 +0900

    feat: LayoutItem 정렬

---
+++ src/components/LayoutItem/LayoutItem.stories.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { ComponentMeta } from '@storybook/react';
+
+import Layout from '../Layout';
+import LayoutItem, { LayoutItemProps } from './LayoutItem';
+
+export default {
+  title: 'component/LayoutItem',
+  component: LayoutItem,
+} as ComponentMeta<typeof LayoutItem>;
+
+export const Default = ({
+  alignSelf,
+  justifySelf,
+}: Pick<LayoutItemProps, 'alignSelf' | 'justifySelf'>): JSX.Element => (
+  <Layout areas="box" rows="200px" columns="200px">
+    <LayoutItem
+      className="w-12 h-12 border border-gray-500"
+      gridArea="box"
+      alignSelf={alignSelf as string}
+      justifySelf={justifySelf as string}
+    >
+      <div>box</div>
+    </LayoutItem>
+  </Layout>
+);
+Default.args = {
+  alignSelf: 'start',
+  justifySelf: 'center',
+};
+Default.argTypes = {
+  alignSelf: {
+    options: ['normal', 'start', 'center', 'end', 'stretch'],
+    control: { type: 'select' },
+  },
+  justifySelf: {
+    options: ['normal', 'start', 'center', 'end', 'stretch'],
+    control: { type: 'select' },
+  },
+};
--- src/components/LayoutItem/LayoutItem.tsx
+++ src/components/LayoutItem/LayoutItem.tsx
@@ -1,18 +1,24 @@
 import * as React from 'react';
 
-interface LayoutItemProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface LayoutItemProps extends React.HTMLAttributes<HTMLDivElement> {
   /** 그리드 아이템에 사용될 명칭 */
   gridArea: React.CSSProperties['gridArea'];
+  /** 아이템의 수직 정렬 */
+  alignSelf?: React.CSSProperties['alignSelf'];
+  /** 아이템의 수평 정렬 */
+  justifySelf?: React.CSSProperties['justifySelf'];
 }
 
 export default function LayoutItem({
   gridArea,
+  alignSelf,
+  justifySelf,
   style,
   children,
   ...args
 }: LayoutItemProps): JSX.Element {
   return (
-    <div style={{ ...style, gridArea }} {...args}>
+    <div style={{ ...style, gridArea, alignSelf, justifySelf }} {...args}>
       {children}
     </div>
   );
