b221c39051be53352ecc409bb01fe758bce217d9
suhyun <chohoki@gmail.com>
2022-04-28 23:02:45 +0900

    feat: implementation Modal

---
+++ src/components/overlay/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Dropdown from './Dropdown';
+
+export default {
+  title: 'overlay/Dropdown',
+  component: Dropdown,
+  args: {
+    data: [
+      { id: 1, title: 'title-1' },
+      { id: 2, title: 'title-2' },
+      { id: 3, title: 'title-3' },
+      { id: 4, title: 'title-4' },
+      { id: 5, title: 'title-5' },
+    ],
+  },
+} as ComponentMeta<typeof Dropdown>;
+
+const Template: ComponentStory<typeof Dropdown> = (args) => (
+  <Dropdown {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  rowKey: 'id',
+  renderItem: (item) => item.title,
+};
